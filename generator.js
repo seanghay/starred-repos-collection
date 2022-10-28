0c25ec09ee843ffbb7423e92331bb3aada68cd0b
Seanghay Yath <seanghay.dev@gmail.com>
2022-10-28 12:11:31 +0700

    add docs

---
+++ generator.js
@@ -0,0 +1,55 @@
+import fs from 'node:fs/promises'
+import _ from 'lodash'
+import fg from 'fast-glob';
+
+const stream = fg.stream('./data/**/*.json');
+
+const topicsCollection = new Map();
+
+for await (const file of stream) {
+
+  /**
+   * gettin' types ;)
+   * @type {Awaited<ReturnType<import('octokit').Octokit['rest']['activity']['listReposStarredByUser']>>['data'][number]}
+   */
+  const repo = JSON.parse(await fs.readFile(file, 'utf-8'));
+  const topics = repo.topics && repo.topics.length > 0 ? repo.topics : [repo.language];
+  
+  for (const topic of topics) {
+    const items = topicsCollection.get(topic) || [];
+    items.push(repo);
+    topicsCollection.set(topic, items);
+  }
+}
+
+const sortedEntries = [...topicsCollection.entries()].sort()
+const sortedMap = Object.fromEntries(sortedEntries);
+const topics = Object.keys(sortedMap);
+
+let markdown = '# Starred Repos Collection\n\n';
+
+markdown += "Organize your starred repos on GitHub into searchable topics.\n\n";
+markdown += "## Table of contents\n\n"
+
+for (const topic of topics) {
+  markdown += ` - ${topic}\n`;
+}
+
+markdown += "\n"
+
+for (const [topic, repos] of sortedEntries) {
+  markdown += `## ${topic} \n\n`;
+
+  for (const repo of repos) {
+    markdown += `### [${repo.full_name}](${repo.html_url}) \n\n`;
+    markdown += `\`${repo.language}\` \n\n`;
+    markdown += `${repo.description} \n\n`;
+  }
+}
+
+
+
+await fs.writeFile('readme.md', markdown, 'utf-8');
+
+// console.log(markdown)
+
